f52ac0e27d56fa1309a3f1ef6c0815c708c8c5b1
Andres Claros <155497035+darkounus90@users.noreply.github.com>
2024-08-23 21:38:29 -0500

    Add files via upload

---
+++ ejercicio1.1/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) {
+        int x = 5;
+        if (x > 10) {
+            System.out.println("x es mayor que 10");
+        } else {
+            System.out.println("x es menor o igual que 10");
+        }
+    }
+}
